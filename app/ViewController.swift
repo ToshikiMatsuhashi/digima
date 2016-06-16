0857c8b5d801f9cc248654c8195e50a3df1b0c31
toshiki matsuhashi <matsuhashi@tapps.jp>
2016-06-16 15:08:19 +0900

    Initial Commit

---
+++ app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by 松橋 俊林 on 6/16/16.
+//  Copyright © 2016 松橋 俊林. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
